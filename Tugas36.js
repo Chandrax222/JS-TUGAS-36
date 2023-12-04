ccb0b345c0405821333a0ccfdcd453cd801b599f
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-12-05 00:14:44 +0700

    External js input selector dan element
    Memanggil ElementID, TagName, Selector dan All

---
+++ Tugas36.js
@@ -0,0 +1,8 @@
+function Tugas36() {
+      
+      console.log(document.getElementById('email'));
+      console.log(document.getElementsByTagName('h2'));
+      console.log(document.querySelectorAll("input[type='password']"));
+      console.log(document.querySelector('.department'));
+}
+Tugas36();
